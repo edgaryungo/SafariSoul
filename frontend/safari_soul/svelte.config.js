03d0db258e34c50cd666ccf119fdf171bdb25dc4
Edgar <eddy8471@gmail.com>
2023-12-08 01:44:59 +0300

    fix deployment

--- frontend/safari_soul/svelte.config.js
+++ frontend/safari_soul/svelte.config.js
@@ -15,7 +15,7 @@ export default {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '200.html',
 			precompress: false,
 			strict: true
 		})
